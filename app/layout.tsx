f41b4fb8bba1cd64b395659337b4e19570826a2b
Ingo Wolf <me@ingowolf.au>
2025-12-11 13:44:45 +1100

    add disclaimer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,23 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
-import './global.css';
-import { Inter } from 'next/font/google';
+import { RootProvider } from "fumadocs-ui/provider/next";
+import "./global.css";
+import { Inter } from "next/font/google";
+import { Banner } from "fumadocs-ui/components/banner";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>
+          <Banner>
+            This app is unofficial. It is independently made and not endorsed or
+            supported by Hack Club.
+          </Banner>
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
